a069ccb47bc83b7c9e6d070c3319747ededf222d
4jeffclark <4jeffclark@gmail.com>
2016-04-06 22:23:08 -0400

    recursive build

--- popJava/build.sh
+++ popJava/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ls
-javac -sourcepath src -d build/classes src/net/homeip/jeffclark/popJava/test/TestAll.java
+#javac -sourcepath src -d build/classes $(find ./src/* | grep .java)src/net/homeip/jeffclark/popJava/test/TestAll.java
+javac -sourcepath src -d build/classes $(find ./src/* | grep .java)
